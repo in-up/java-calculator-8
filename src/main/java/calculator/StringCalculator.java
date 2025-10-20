80a4536dd83f22d3c2f2efd581058382d0a2de93
in-up <inup@kyonggi.ac.kr>
2025-10-20 21:17:56 +0900

    feat(calculator): 구분자 처리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,13 +1,40 @@
 package calculator;
 
 public class StringCalculator {
+    private static final String DEFAULT_DELIMS = "[,:]";
+
     public static int add(String input) {
         // 공백 문자열 처리
         if (input == null || input.isBlank()) {
             return 0;
         }
-        // TODO: 문자열에서 숫자를 추출하여 더하는 계산기를 구현
+
+        // 단일 숫자 처리
+        if (input.matches("\\d+")) {
+            return Integer.parseInt(input);
+        }
+
+        // 기본 구분자 처리
+        if (containsDefaultDelimiter(input)) {
+            String[] tokens = input.split(DEFAULT_DELIMS);
+            int sum = 0;
+            for (String token : tokens) {
+                // TODO: 예외 정책
+                sum += Integer.parseInt(token);
+            }
+            return sum;
+        }
+
+        // TODO: 커스텀 구분자 처리
         // TODO: 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException 처리
+
         throw new IllegalArgumentException("Illegal Argument Exception: " + input);
+    }
+
+    private static boolean containsDefaultDelimiter(String s) {
+        return s.indexOf(',') >= 0 || s.indexOf(':') >= 0;
+    }
+
+    private StringCalculator() {
     }
 }
